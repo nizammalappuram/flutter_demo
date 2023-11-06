a23b86de81bd4ab71a7066d069a0e883cb62a119
nizam <officialark11@gmail.com>
2023-11-07 02:18:56 +0530

    new modified

--- demo/lib/screens/signup_screen.dart
+++ demo/lib/screens/signup_screen.dart
@@ -1,12 +1,223 @@
 
 
+// import 'package:flutter/material.dart';
+// import 'package:demo/services/user_service.dart';
+// import 'package:demo/app_style.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:demo/screens/login_screen.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
+
+
+// class Signup extends StatefulWidget {
+//   static const String id = "/signup";
+
+//   @override
+//   _SignupState createState() => _SignupState();
+// }
+
+// class _SignupState extends State<Signup> {
+//   final TextEditingController emailController = TextEditingController();
+//   final TextEditingController usernameController = TextEditingController();
+//   final TextEditingController passwordController = TextEditingController();
+//   final TextEditingController confirmPasswordController = TextEditingController();
+
+//   Future<void> registerUser() async {
+//     final String email = emailController.text;
+//     final String username = usernameController.text;
+//     final String password = passwordController.text;
+
+//     final userData = await APIService.registerUser(email, username, password);
+
+
+//     if (userData != null) {
+//       print('User registered successfully');
+//       // Navigate to the login screen
+//       Navigator.of(context).pushReplacementNamed(Login.id);
+//     } else {
+//       print('Failed to register user');
+//       // Handle errors or display an error message
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     Size size = MediaQuery.of(context).size;
+//     return Scaffold(
+//       body: SingleChildScrollView(
+//         child: SafeArea(
+//           child: Padding(
+//             padding: EdgeInsets.symmetric(
+//               horizontal: size.width * 0.1,
+//               vertical: size.height * 0.001,
+//             ),
+//             child: Column(
+//               children: [
+//               Align(
+//               alignment: Alignment.topCenter,
+//               child: Image.asset(
+//                 logoImage, // Replace with your actual logo image path
+//                 height: size.height * 0.1,
+//               ),
+//             ),
+//             SizedBox(height: size.height * 0.023),
+//             Text(
+//               "Sign Up",
+//               style: Theme.of(context).textTheme.titleLarge,
+//             ),
+//             SizedBox(height: size.height * 0.018),
+//             Text(
+//               "Create a new account",
+//               style: Theme.of(context)
+//                   .textTheme
+//                   .titleSmall!
+//                   .copyWith(fontSize: 15),
+//             ),
+//             SizedBox(height: size.height * 0.020),
+//             TextField(
+//               controller: emailController,
+//               style: const TextStyle(color: kLightTextColor),
+//               decoration: InputDecoration(
+//                 hintText: "Email",
+//                 prefixIcon: IconButton(
+//                   onPressed: null,
+//                   icon: SvgPicture.asset(
+//                     'assets/icons/email.svg', // Replace with your email icon path
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             SizedBox(height: size.height * 0.016),
+//             TextField(
+//               controller: usernameController,
+//               style: const TextStyle(color: kLightTextColor),
+//               decoration: InputDecoration(
+//                 hintText: "Username",
+//                 prefixIcon: IconButton(
+//                   onPressed: null,
+//                   icon: SvgPicture.asset(
+//                     'assets/icons/user.svg', // Replace with your user icon path
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             SizedBox(height: size.height * 0.016),
+//             TextField(
+//               controller: passwordController,
+//               obscureText: true,
+//               style: const TextStyle(color: kLightTextColor),
+//               decoration: InputDecoration(
+//                 hintText: "Password",
+//                 prefixIcon: IconButton(
+//                   onPressed: null,
+//                   icon: SvgPicture.asset(
+//                     'assets/icons/lock.svg', // Replace with your lock icon path
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             SizedBox(height: size.height * 0.016),
+//             TextField(
+//               controller: confirmPasswordController,
+//               obscureText: true,
+//               style: const TextStyle(color: kLightTextColor),
+//               decoration: InputDecoration(
+//                 hintText: "Confirm Password",
+//                 prefixIcon: IconButton(
+//                   onPressed: null,
+//                   icon: SvgPicture.asset(
+//                     'assets/icons/lock.svg', // Replace with your lock icon path
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             SizedBox(height: size.height * 0.025),
+//             ElevatedButton(
+//               onPressed: () {
+//                 if (passwordController.text == confirmPasswordController.text) {
+//                   registerUser();
+//                 } else {
+//                   print("Passwords don't match.");
+//                 }
+//               },
+//               child: Text(
+//                 "Sign Up",
+//                 style: Theme.of(context).textTheme.titleMedium,
+//               ),
+//             ),
+//             SizedBox(height: size.height * 0.034),
+//             Row(
+//               children: [
+//                 const Expanded(child: Divider(color: kLightTextColor)),
+//                 Padding(
+//                   padding: const EdgeInsets.symmetric(horizontal: 10),
+//                   child: Text(
+//                     "or Sign Up with Google",
+//                     style: Theme.of(context).textTheme.titleSmall,
+//                   ),
+//                 ),
+//                 const Expanded(child: Divider(color: kLightTextColor)),
+//               ],
+//             ),
+//             SizedBox(height: size.height * 0.025),
+//             ElevatedButton(
+//               onPressed: () {},
+//               style: ElevatedButton.styleFrom(
+//                 primary: kWhiteColor, // Use 'primary' to set the background color
+//                 elevation: 0,
+//               ),
+//               child: Text(
+//                 "Sign Up with Google",
+//                 style: Theme.of(context)
+//                     .textTheme
+//                     .titleSmall!
+//                     .copyWith(fontSize: 16, color: kBlackColor),
+//               ),
+//             ),
+//             SizedBox(height: size.height * 0.041),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Text(
+//                   "Already a member?\t",
+//                   style: Theme.of(context).textTheme.titleSmall,
+//                 ),
+//                 CupertinoButton(
+//                   padding: EdgeInsets.zero,
+//                   onPressed: () {
+//                     Navigator.of(context).pushNamedAndRemoveUntil(
+//                         Login.id, (route) => false);
+//                   },
+//                   child: Text(
+//                     "Sign In",
+//                     style: Theme.of(context)
+//                         .textTheme
+//                         .titleSmall!
+//                         .copyWith(color: kTextColor),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+// -----------------------------------------------------------------------------
+
+// test code
+
+
 import 'package:flutter/material.dart';
 import 'package:demo/services/user_service.dart';
-import 'package:demo/app_style.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:demo/screens/login_screen.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
 
 class Signup extends StatefulWidget {
   static const String id = "/signup";
@@ -21,17 +232,23 @@ class _SignupState extends State<Signup> {
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController confirmPasswordController = TextEditingController();
 
+  List<String> roles = ['student', 'academy', 'mentor', 'trainer'];
+  String? selectedRole;
+
   Future<void> registerUser() async {
+    if (selectedRole == null) {
+      print('Please select a role');
+      return;
+    }
+
     final String email = emailController.text;
     final String username = usernameController.text;
     final String password = passwordController.text;
 
-    final userData = await APIService.registerUser(email, username, password);
-
+    final userData = await APIService.registerUser(email, username, password, selectedRole!);
 
     if (userData != null) {
       print('User registered successfully');
-      // Navigate to the login screen
       Navigator.of(context).pushReplacementNamed(Login.id);
     } else {
       print('Failed to register user');
@@ -52,151 +269,102 @@ class _SignupState extends State<Signup> {
             ),
             child: Column(
               children: [
-              Align(
-              alignment: Alignment.topCenter,
-              child: Image.asset(
-                logoImage, // Replace with your actual logo image path
-                height: size.height * 0.1,
-              ),
-            ),
-            SizedBox(height: size.height * 0.023),
-            Text(
-              "Sign Up",
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            SizedBox(height: size.height * 0.018),
-            Text(
-              "Create a new account",
-              style: Theme.of(context)
-                  .textTheme
-                  .titleSmall!
-                  .copyWith(fontSize: 15),
-            ),
-            SizedBox(height: size.height * 0.020),
-            TextField(
-              controller: emailController,
-              style: const TextStyle(color: kLightTextColor),
-              decoration: InputDecoration(
-                hintText: "Email",
-                prefixIcon: IconButton(
-                  onPressed: null,
-                  icon: SvgPicture.asset(
-                    'assets/icons/email.svg', // Replace with your email icon path
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Image.asset(
+                    'assets/images/logo.png', // Replace with your actual logo image path
+                    height: size.height * 0.1,
                   ),
                 ),
-              ),
-            ),
-            SizedBox(height: size.height * 0.016),
-            TextField(
-              controller: usernameController,
-              style: const TextStyle(color: kLightTextColor),
-              decoration: InputDecoration(
-                hintText: "Username",
-                prefixIcon: IconButton(
-                  onPressed: null,
-                  icon: SvgPicture.asset(
-                    'assets/icons/user.svg', // Replace with your user icon path
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: size.height * 0.016),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              style: const TextStyle(color: kLightTextColor),
-              decoration: InputDecoration(
-                hintText: "Password",
-                prefixIcon: IconButton(
-                  onPressed: null,
-                  icon: SvgPicture.asset(
-                    'assets/icons/lock.svg', // Replace with your lock icon path
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: size.height * 0.016),
-            TextField(
-              controller: confirmPasswordController,
-              obscureText: true,
-              style: const TextStyle(color: kLightTextColor),
-              decoration: InputDecoration(
-                hintText: "Confirm Password",
-                prefixIcon: IconButton(
-                  onPressed: null,
-                  icon: SvgPicture.asset(
-                    'assets/icons/lock.svg', // Replace with your lock icon path
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: size.height * 0.025),
-            ElevatedButton(
-              onPressed: () {
-                if (passwordController.text == confirmPasswordController.text) {
-                  registerUser();
-                } else {
-                  print("Passwords don't match.");
-                }
-              },
-              child: Text(
-                "Sign Up",
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-            ),
-            SizedBox(height: size.height * 0.034),
-            Row(
-              children: [
-                const Expanded(child: Divider(color: kLightTextColor)),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Text(
-                    "or Sign Up with Google",
-                    style: Theme.of(context).textTheme.titleSmall,
-                  ),
-                ),
-                const Expanded(child: Divider(color: kLightTextColor)),
-              ],
-            ),
-            SizedBox(height: size.height * 0.025),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                primary: kWhiteColor, // Use 'primary' to set the background color
-                elevation: 0,
-              ),
-              child: Text(
-                "Sign Up with Google",
-                style: Theme.of(context)
-                    .textTheme
-                    .titleSmall!
-                    .copyWith(fontSize: 16, color: kBlackColor),
-              ),
-            ),
-            SizedBox(height: size.height * 0.041),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
+                SizedBox(height: size.height * 0.023),
                 Text(
-                  "Already a member?\t",
-                  style: Theme.of(context).textTheme.titleSmall,
+                  "Sign Up",
+                  style: Theme.of(context).textTheme.headline5,
                 ),
-                CupertinoButton(
-                  padding: EdgeInsets.zero,
-                  onPressed: () {
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                        Login.id, (route) => false);
-                  },
-                  child: Text(
-                    "Sign In",
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleSmall!
-                        .copyWith(color: kTextColor),
+                SizedBox(height: size.height * 0.018),
+                Text(
+                  "Create a new account",
+                  style: Theme.of(context).textTheme.subtitle1,
+                ),
+                SizedBox(height: size.height * 0.020),
+                TextField(
+                  controller: emailController,
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    prefixIcon: IconButton(
+                      onPressed: null,
+                      icon: SvgPicture.asset(
+                        'assets/icons/email.svg', // Replace with your email icon path
+                      ),
+                    ),
                   ),
                 ),
-              ],
-            ),
+                SizedBox(height: size.height * 0.016),
+                TextField(
+                  controller: usernameController,
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    hintText: "Username",
+                    prefixIcon: IconButton(
+                      onPressed: null,
+                      icon: SvgPicture.asset(
+                        'assets/icons/user.svg', // Replace with your user icon path
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: size.height * 0.016),
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    prefixIcon: IconButton(
+                      onPressed: null,
+                      icon: SvgPicture.asset(
+                        'assets/icons/lock.svg', // Replace with your lock icon path
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: size.height * 0.016),
+                TextField(
+                  controller: confirmPasswordController,
+                  obscureText: true,
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    hintText: "Confirm Password",
+                    prefixIcon: IconButton(
+                      onPressed: null,
+                      icon: SvgPicture.asset(
+                        'assets/icons/lock.svg', // Replace with your lock icon path
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: size.height * 0.025),
+                DropdownButton<String>(
+                  value: selectedRole,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedRole = newValue;
+                    });
+                  },
+                  items: roles.map((String role) {
+                    return DropdownMenuItem<String>(
+                      value: role,
+                      child: Text(role),
+                    );
+                  }).toList(),
+                ),
+                SizedBox(height: size.height * 0.025),
+                ElevatedButton(
+                  onPressed: registerUser,
+                  child: Text("Sign Up", style: TextStyle(color: Colors.white)),
+                ),
+                // Other UI components...
               ],
             ),
           ),
--- demo/lib/services/user_service.dart
+++ demo/lib/services/user_service.dart
@@ -1,4 +1,3 @@
-
 // import 'package:http/http.dart' as http;
 // import 'dart:convert';
 
@@ -37,21 +36,99 @@
 //   }
 // }
 
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
 
+// class APIService {
+//   static Future<Map<String, dynamic>?> registerUser(
+//       String email, String username, String password) async {
+//     final String registerUrl = 'http://10.0.2.2:5000/student/signup'; // Replace with your register API URL
 
+//     final Map<String, dynamic> registerData = {
+//       'email': email,
+//       'username': username,
+//       'password': password,
+//       'roles': 'student',
+//     };
+
+//     try {
+//       final registerResponse = await http.post(
+//         Uri.parse(registerUrl),
+//         body: json.encode(registerData),
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//       );
+
+//       if (registerResponse.statusCode == 200) {
+//         final userData = jsonDecode(registerResponse.body);
+//         return userData;
+//       } else {
+//         print('Failed to register user. Status code: ${registerResponse.statusCode}');
+//         return null;
+//       }
+//     } catch (e) {
+//       print('Error: $e');
+//       return null;
+//     }
+//   }
+
+//   static Future<Map<String, dynamic>?> loginUser(
+//       String email, String password) async {
+//     final String loginUrl = 'http://10.0.2.2:5000/student/login'; // Replace with your login API URL
+
+//     final Map<String, dynamic> loginData = {
+//       'email': email,
+//       'password': password,
+//     };
+
+//     try {
+//       final loginResponse = await http.post(
+//         Uri.parse(loginUrl),
+//         body: json.encode(loginData),
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//       );
+
+//       if (loginResponse.statusCode == 200) {
+//         final userData = jsonDecode(loginResponse.body);
+//         return userData;
+//       } else {
+//         print('Failed to log in. Status code: ${loginResponse.statusCode}');
+//         return null;
+//       }
+//     } catch (e) {
+//       print('Error: $e');
+//       return null;
+//     }
+//   }
+// }
+
+// ----------------------------------------------------------------------
+// test api
+
+// user_service.dart
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class APIService {
-  static Future<Map<String, dynamic>?> registerUser(
-      String email, String username, String password) async {
-    final String registerUrl = 'http://localhost:5000/student/signup'; // Replace with your register API URL
+  static var selectedRole;
+
+  static Future<Map<String, dynamic>?> registerUser(String email,
+      String username, String password, String? selectedRole) async {
+    final String registerUrl = 'http://localhost:5000/$selectedRole/signup';
+
+    if (selectedRole == null || selectedRole.isEmpty) {
+      print('No role selected');
+      return null;
+    }
 
     final Map<String, dynamic> registerData = {
       'email': email,
       'username': username,
       'password': password,
-      'roles': 'student',
+      'roles': selectedRole,
     };
 
     try {
@@ -64,10 +141,10 @@ class APIService {
       );
 
       if (registerResponse.statusCode == 200) {
-        final userData = jsonDecode(registerResponse.body);
-        return userData;
+        return jsonDecode(registerResponse.body);
       } else {
-        print('Failed to register user. Status code: ${registerResponse.statusCode}');
+        print(
+            'Failed to register user. Status code: ${registerResponse.statusCode}');
         return null;
       }
     } catch (e) {
@@ -77,11 +154,11 @@ class APIService {
   }
 
   static Future<Map<String, dynamic>?> loginUser(
-      String email, String password) async {
-    final String loginUrl = 'http://localhost:5000/student/login'; // Replace with your login API URL
+      String username, String password, String selectedRole) async {
+    final String loginUrl = 'http://localhost:5000/$selectedRole/login';
 
     final Map<String, dynamic> loginData = {
-      'email': email,
+      'username': username,
       'password': password,
     };
 
@@ -95,8 +172,7 @@ class APIService {
       );
 
       if (loginResponse.statusCode == 200) {
-        final userData = jsonDecode(loginResponse.body);
-        return userData;
+        return jsonDecode(loginResponse.body);
       } else {
         print('Failed to log in. Status code: ${loginResponse.statusCode}');
         return null;
@@ -107,4 +183,3 @@ class APIService {
     }
   }
 }
-
